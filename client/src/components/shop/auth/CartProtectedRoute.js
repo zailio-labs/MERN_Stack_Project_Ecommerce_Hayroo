82a458777a1f16c8cbf530ac6e46142956d163bd
Zailio Labs <100409086+zailio-labs@users.noreply.github.com>
2026-01-16 01:28:12 +0530

    Update CartProtectedRoute.js

--- client/src/components/shop/auth/CartProtectedRoute.js
+++ client/src/components/shop/auth/CartProtectedRoute.js
@@ -1,24 +1,15 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { isAuthenticate } from "./fetchApi";
 
-const CartProtectedRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      JSON.parse(localStorage.getItem("cart")).length !== 0 &&
-      isAuthenticate() ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: "/",
-            state: { from: props.location },
-          }}
-        />
-      )
-    }
-  />
-);
+const CartProtectedRoute = ({ children }) => {
+  const cart = localStorage.getItem("cart");
+  
+  if (cart && JSON.parse(cart).length !== 0 && isAuthenticate()) {
+    return children;
+  }
+  
+  return <Navigate to="/" replace />;
+};
 
 export default CartProtectedRoute;
