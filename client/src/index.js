eb0eca2eaa74b2122942e5904a912301f4adbfbc
Zailio Labs <100409086+zailio-labs@users.noreply.github.com>
2026-01-16 01:25:39 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import {SnackbarProvider} from 'notistack'
-ReactDOM.render(
+import { SnackbarProvider } from 'notistack';
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
-<SnackbarProvider maxSnack={3}> 
-    <App />
+    <SnackbarProvider maxSnack={3}> 
+      <App />
     </SnackbarProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 serviceWorker.unregister();
